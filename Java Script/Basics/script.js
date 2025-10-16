b8e561f1cf73f139bb31adb1af5ea950d7376f01
Anamika <anamikakumari.71611@gmail.com>
2025-10-16 13:58:21 +0530

    sum

--- Java Script/Basics/script.js
+++ Java Script/Basics/script.js
@@ -13,8 +13,25 @@
 //     return a+b;
 // }
 
-const greeting = (username = "stupid") =>{
-    console.log("hello ! " + username)
-};
+// const greeting = (username = "stupid") =>{
+//     console.log("hello ! " + username)
+// };
 
-greeting();
+// greeting();
+
+// const sum = (num1, num2 ,...rest)=>{
+//     console.log(num1);
+//     console.log(num2);
+//     console.log(rest);
+// };
+
+// sum(1,2,9,8,5,6);
+
+const sum = (num1, num2, ...rest) => {
+    let result = num1 + num2;
+    for(let i = 0; i< rest.length; i++){
+        result = result + rest[i];
+    }
+    console.log(result);
+}
+sum(3,4,5,6,6,7,7,88,8,5)
