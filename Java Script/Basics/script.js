8b1110eb48e6de76218c14cbf304128b3f3d4ad1
Anamika <anamikakumari.71611@gmail.com>
2025-10-16 13:28:58 +0530

    function

--- Java Script/Basics/script.js
+++ Java Script/Basics/script.js
@@ -1,5 +1,14 @@
-if("hi"){ //considered true
-    console.log('Hello')
-}else{
-    console.log('Bye')
+
+// const sum = function(c,d){
+//     return c+d;
+// };
+
+console.log(sum(3,4));
+
+// const sum = (e,f) => {
+//     return e+f;
+// };
+
+function sum(a,b){
+    return a+b;
 }
